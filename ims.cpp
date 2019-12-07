36840a50746fed95f216e81382a1dbcfefd77253
Tomáš Sasák <tsasak@redhat.com>
2019-12-07 18:17:22 +0100

    Fix wrong calculation

--- ims.cpp
+++ ims.cpp
@@ -10,7 +10,6 @@ double cornGrain = 0;
 double cornStover = 0;
 long long unsigned dayWatch = 50;
 
-Store DieselFactories(100);
 Store FiberFactories(100);
 Store HarvestCompany(50);
 Store CornBioplasticFactory(50);
@@ -21,9 +20,8 @@ Stat bioPetrol("Biopetrol created");
 Stat bioTurboDiesel("Bioturbodiesel created");
 Stat bioPlasticsCorn("Bioplastics created by corn");
 Stat bioPlasticsHemp("Bioplastics created by hemp");
+Stat burntPlastics("Plastics that were burnt");
 
-#define LOWEST_PLASTICS_OIL 13
-#define HIGHEST_PLASTICS_OIL 15
 
 class WatchDog : public Event
 {
@@ -34,6 +32,7 @@ public:
         std::cout << "Currently there is " << bioPlastics.Sum() << " Tons of bioplastics \n";
         std::cout << "Currently there is " << bioPlasticsCorn.Sum() << " Tons of bioplastics made from corn \n";
         std::cout << "Currently there is " << bioPlasticsHemp.Sum() << " Tons of bioplastics made from hemp \n";
+        std::cout << "There were burnt " << burntPlastics.Sum() << " Tons of bioplastics\n";
         std::cout << "Currently there is " << bioDiesel.Sum() << " Litres of biodiesel \n";
         std::cout << "Currently there is " << bioPetrol.Sum() << " Litres of biopetrol \n";
         std::cout << "Currently there is " << bioTurboDiesel.Sum() << " Litres of bioturbodiesel \n";
@@ -53,15 +52,16 @@ public:
             if(landfillPlastics < 13)
             {
                 // Take the remaining plastics
+                burntPlastics(landfillPlastics);
                 landfillPlastics = 0;
             }
             else
             {
                 // Roll between 13 - 15 Tons of plastics, that it take to create oils
-                landfillPlastics -= Uniform(LOWEST_PLASTICS_OIL, HIGHEST_PLASTICS_OIL);
+                double burnt = Uniform(13, 15);
+                burntPlastics(burnt);
+                landfillPlastics -= burnt;
             }
-
-            Enter(DieselFactories, 1);
             
             // It takes over month to create oil
             Wait(Uniform(28, 31));
@@ -74,8 +74,6 @@ public:
 
             // 40 - 50 litres of biopetrol
             bioPetrol(50);
-
-            Leave(DieselFactories, 1);
         }
     }
 };
@@ -99,6 +97,7 @@ public:
             bioPlastics(rawHemp);
             rawHemp = 0;
         }
+        // 1 Ton of hemp fiber takes 2 - 8 hours to create
         // Else make 20 tons 
         // (Yes, documentation talks about 1 ton per 2 - 8 hours, but we needed to multiply the count, otherwise the calculation
         //  would take too long)
@@ -108,10 +107,8 @@ public:
             bioPlastics(20);
             rawHemp -= 20;
         }
-        // 1 Ton of hemp fiber takes 2 - 8 hours to create
         Wait(Uniform(1.6, 6.6));
         
-
         // End the job
         Leave(FiberFactories, 1);
     }
@@ -123,14 +120,13 @@ public:
     void Behavior()
     {
         // Take the unproccessed hemp and put it to the hemp
-        rawHemp += 44.6;
+        rawHemp += 0.54;
 
         // Perform next growing
         Activate(Time + Uniform(70, 140));
 
         // Call factory for each ton to process the raw hemp to fiber
-        if(rawHemp >= 20)
-            (new FiberFactoryProc)->Activate();
+        (new FiberFactoryProc)->Activate();
     }
 };
 
@@ -146,8 +142,13 @@ public:
         Activate(Time + Uniform(70, 140));
 
         // Call factory to process the raw hemp to fiber
-        if(rawHemp >= 20)
+        double createdHemp = 135;
+        while(createdHemp > 20)
+        {
             (new FiberFactoryProc)->Activate();
+            createdHemp -= 20;
+        }
+        (new FiberFactoryProc)->Activate();
     }
 };
 
@@ -234,6 +235,7 @@ int main(int argc, char *argv[])
     std::cout << std::fixed;
     char option;
 
+    // default values
     long unsigned days = 365;
     long unsigned cbdFarms = 723;
     long unsigned hempFarms = 1687;
@@ -303,22 +305,22 @@ int main(int argc, char *argv[])
     // Run simulations
     Run();
 
-
     bioPlastics.Output();
+    burntPlastics.Output();
     bioDiesel.Output();
     bioPetrol.Output();
     bioTurboDiesel.Output();
-    DieselFactories.Output();
     FiberFactories.Output();
 
     std::cout << "Simulation ran for " << Time << " days (" << (Time / 365) << "years)\n";
     std::cout << "There were created " << bioPlastics.Sum() << " Tons of bioplastics \n";
+    std::cout << "There were burnt " << burntPlastics.Sum() << " Tons of bioplastics\n";
     std::cout << "Created bioplastics with corn " << bioPlasticsCorn.Sum() << " Tons of bioplastics \n";
     std::cout << "Created bioplastics with hemp " << bioPlasticsHemp.Sum() << " Tons of bioplastics \n";
     std::cout << "There were created " << bioDiesel.Sum() << " Litres of biodiesel \n";
     std::cout << "There were created " << bioPetrol.Sum() << " Litres of biopetrol \n";
     std::cout << "There were created " << bioTurboDiesel.Sum() << " Litres of bioturbodiesel \n";
-    std::cout << "There are: " << landfillPlastics << " Tons of plastics left on lanfill" << "\n";
+    std::cout << "There are: " << landfillPlastics << " Tons of plastics left on landfill" << "\n";
 
     return 0;
 }
